1d5395d4d49dfe0ee783a0e7c8040058d7c1c290
Andrey <krosser006@mail.ru>
2021-12-30 14:23:12 +0300

    Отображение случайных картинок

--- game.py
+++ game.py
@@ -4,21 +4,16 @@ from PyQt5 import uic  # Импортируем uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QRadioButton
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QSize
+import random
+
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)  # Загружаем дизайн
         self.images = os.listdir("images")
-        print(self.images)
-        self.names = ['Cls', 'Bck', '', 'Close',
-                      '7', '8', '9', '/',
-                      '4', '5', '6', '*',
-                      '1', '2', '3', '-',
-                      '0', '.', '=', '+']
+        random.shuffle(self.images)
         self.buttonGroup.buttonClicked.connect(self.complexity_change)
-
-
 
     def complexity_change(self):
         radio = self.sender()
@@ -33,7 +28,7 @@ class MyWidget(QMainWindow):
 
     def create_fild(self, complexity):
         self.deleteAll()
-        self.start_text.isVisible = False
+
         button_size = 60
         n = -1
         if complexity == 0:
@@ -45,13 +40,17 @@ class MyWidget(QMainWindow):
         elif complexity == 2:
             n = 8
 
+        my_images = self.images[:n * 2]
+        my_images = my_images + my_images
+        random.shuffle(my_images)
 
         positions = [(i, j) for i in range(n) for j in range(n)]
         for position in positions:
             button = QPushButton(self)
             button.setFixedWidth(button_size)
             button.setFixedHeight(button_size)
-            button.setIcon(QIcon(f"images/{self.images[position[0]]}"))
+
+            button.setIcon(QIcon(f"images/{my_images.pop()}"))
             button.setIconSize(QSize(50, 50))
             self.grid.addWidget(button, *position)
 
